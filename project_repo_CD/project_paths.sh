76e93cd7e0dc7bd3b94214a174c0ce78daa47d47
dmedin2 <daniel.medina.villegas@intel.com>
2019-06-07 09:11:17 -0700

    Project Repo CD removal of non generic path
    Change-Id: Ie6ba41665a1610cfed3c16d956c4d3a7e9ea4ac8

--- project_repo_CD/project_paths.sh
+++ project_repo_CD/project_paths.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-python $PPC/home/dmv_home/scripts/project_paths.py --req_path $1
+# Adjust this call to the correct python script
+python scripts/project_paths.py --req_path $1
 cd `cat ~/.repo_path`
 #echo -n > $HOME/.repo_path
